6b2080a1b06abc40e0f6964b398b75f7befd5e94
Simon Luo <simonlaw910@gmail.com>
2022-06-10 22:22:40 +0800

    feat: add initial server config

---
+++ src/types/index.ts
@@ -0,0 +1,23 @@
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
+
+export type Method =
+  'get'
+  | 'GET'
+  | 'post'
+  | 'POST'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
