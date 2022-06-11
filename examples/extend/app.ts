f17a1402c2ea0ae52c6665e0e5ef7134a90bdab0
Simon Luo <simonlaw910@gmail.com>
2022-06-11 22:28:18 +0800

    feat: add axios override function

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -30,3 +30,18 @@ axios.post('/extend/post', { msg: 'post' })
 axios.put('/extend/put', { msg: 'put' })
 
 axios.patch('/extend/put', { msg: 'patch' })
+
+axios({
+  url: '/extend/post',
+  method: 'post',
+  data: {
+    msg: 'hi'
+  }
+})
+
+axios('/extend/post', {
+  method: 'post',
+  data: {
+    msg: 'hello'
+  }
+})
--- src/types/index.ts
+++ src/types/index.ts
@@ -18,6 +18,8 @@ export interface Axios {
 
 export interface AxiosInstance extends Axios {
   (config: AxiosRequestConfig): AxiosPromise
+
+  (url: string, config?: AxiosRequestConfig): AxiosPromise
 }
 
 export interface AxiosRequestConfig {
