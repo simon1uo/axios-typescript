e5058f7e7aa9c106f1f8a4f0818ce4d0f8ddf920
Simon Luo <simonlaw910@gmail.com>
2022-06-17 09:58:49 +0800

    feat: add axios create instance extension

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -16,17 +16,50 @@ axios.defaults.headers.common['test2'] = 123
   console.log(res.data)
 })*/
 
-
 axios({
-  transformRequest: [(function(data) {
-    return qs.stringify(data)
-  }), ...(axios.defaults.transformRequest as AxiosTransformer[])],
-  transformResponse: [...(axios.defaults.transformResponse as AxiosTransformer[]), function(data) {
-    if (typeof data === 'object') {
-      data.b = 2
+  transformRequest: [
+    function(data) {
+      return qs.stringify(data)
+    },
+    ...(axios.defaults.transformRequest as AxiosTransformer[])
+  ],
+  transformResponse: [
+    ...(axios.defaults.transformResponse as AxiosTransformer[]),
+    function(data) {
+      if (typeof data === 'object') {
+        data.b = 2
+      }
+      return data
     }
-    return data
-  }],
+  ],
+  url: '/config/post',
+  method: 'post',
+  data: {
+    a: 1
+  }
+}).then(res => {
+  console.log(res.data)
+})
+
+const instance = axios.create({
+  transformRequest: [
+    function(data) {
+      return qs.stringify(data)
+    },
+    ...(axios.defaults.transformRequest as AxiosTransformer[])
+  ],
+  transformResponse: [
+    ...(axios.defaults.transformResponse as AxiosTransformer[]),
+    function(data) {
+      if (typeof data === 'object') {
+        data.b = 2
+      }
+      return data
+    }
+  ]
+})
+
+instance({
   url: '/config/post',
   method: 'post',
   data: {
--- src/types/index.ts
+++ src/types/index.ts
@@ -94,3 +94,7 @@ export interface RejectedFn {
 export interface AxiosTransformer {
   (data: any, headers?: any): any
 }
+
+export interface AxiosStatic extends AxiosInstance {
+  create(config?: AxiosRequestConfig): AxiosInstance
+}
